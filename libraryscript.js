01d826f24eb19553f245f9baa4dc538be2d42fc2
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-05-28 00:07:41 +0200

    made the popup work

--- libraryscript.js
+++ libraryscript.js
@@ -1,1 +1,12 @@
 "use strict";
+const popUp = document.querySelector('.pop-up');
+const overlay = document.querySelector('.overlay');
+const addBook = document.querySelector('.add-book');
+addBook === null || addBook === void 0 ? void 0 : addBook.addEventListener('click', () => {
+    popUp === null || popUp === void 0 ? void 0 : popUp.classList.add('active');
+    overlay === null || overlay === void 0 ? void 0 : overlay.classList.add('active');
+});
+overlay === null || overlay === void 0 ? void 0 : overlay.addEventListener('click', () => {
+    popUp === null || popUp === void 0 ? void 0 : popUp.classList.remove('active');
+    overlay.classList.remove('active');
+});
--- libraryscript.ts
+++ libraryscript.ts
@@ -1,1 +1,14 @@
-let myLibrary: string[] = [];
+const popUp = document.querySelector('.pop-up');
+const overlay = document.querySelector('.overlay');
+const addBook = document.querySelector('.add-book');
+
+
+addBook?.addEventListener('click', () => {
+    popUp?.classList.add('active');
+    overlay?.classList.add('active');
+})
+
+overlay?.addEventListener('click', () => {
+    popUp?.classList.remove('active');
+    overlay.classList.remove('active');
+})
