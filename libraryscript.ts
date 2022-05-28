02c653475eeca638513ce12cdc04578f88853e4b
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-05-28 23:06:11 +0200

    added colourRandomizer and randomInt function

--- libraryscript.ts
+++ libraryscript.ts
@@ -84,3 +84,16 @@ function addCard() {
     /*bookCardContainer.appendChild(ReadorNot);*/
 }
 // console.log(myLibrary);
+
+function colourRandomizer() {
+    let colourArr: string[] = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+    let randomColourStr: string = "";
+    for(let i = 0; i < 6; i++) {
+        randomColourStr += colourArr[randomInt(0,15)];
+    }
+    return randomColourStr;
+}
+
+function randomInt(max: number, min: number):number {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
