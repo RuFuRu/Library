f9bd2a09f1a5876e442ca4f322387c6848db0eb2
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-05-28 21:46:10 +0200

    adding a way for adding items into the library

--- libraryscript.js
+++ libraryscript.js
@@ -1,21 +1,39 @@
 "use strict";
 const popUp = document.querySelector('.pop-up');
 const overlay = document.querySelector('.overlay');
-const addBook = document.querySelector('.add-book');
-addBook === null || addBook === void 0 ? void 0 : addBook.addEventListener('click', () => {
-    popUp === null || popUp === void 0 ? void 0 : popUp.classList.add('active');
-    overlay === null || overlay === void 0 ? void 0 : overlay.classList.add('active');
+const addBookBtn = document.querySelector('.add-book');
+const popUpBtn = document.querySelector('.pop-up-btn');
+addBookBtn.addEventListener('click', () => {
+    popUp.classList.add('active');
+    overlay.classList.add('active');
 });
-overlay === null || overlay === void 0 ? void 0 : overlay.addEventListener('click', () => {
-    popUp === null || popUp === void 0 ? void 0 : popUp.classList.remove('active');
+overlay.addEventListener('click', () => {
+    popUp.classList.remove('active');
     overlay.classList.remove('active');
 });
+popUpBtn.addEventListener('click', () => {
+    addBook();
+});
+let myLibrary = [];
 class Book {
-    constructor(author, title, genre, numOfPages, read) {
+    constructor(author, title, genre, numOfPages, readYes, readNo) {
         this.author = author;
         this.title = title;
         this.genre = genre;
         this.numOfPages = numOfPages;
-        this.read = read;
+        this.readYes = readYes;
+        this.readNo = readNo;
     }
 }
+function addBook() {
+    const author = document.querySelector('#author');
+    const title = document.querySelector('#title');
+    const genre = document.querySelector('#pageNum');
+    const pageNum = document.querySelector('#pageNum');
+    const answerYes = document.querySelector('.yes');
+    const answerNo = document.querySelector('.no');
+    let object = new Book(author.value, title.value, genre.value, pageNum.valueAsNumber, answerYes.checked, answerNo.checked);
+    myLibrary.push(object);
+    console.log(myLibrary);
+}
+// console.log(myLibrary);
--- libraryscript.ts
+++ libraryscript.ts
@@ -1,18 +1,24 @@
-const popUp = document.querySelector('.pop-up');
-const overlay = document.querySelector('.overlay');
-const addBook = document.querySelector('.add-book');
+const popUp = document.querySelector('.pop-up') as HTMLDivElement;
+const overlay = document.querySelector('.overlay') as HTMLDivElement;
+const addBookBtn = document.querySelector('.add-book') as HTMLButtonElement;
+const popUpBtn = document.querySelector('.pop-up-btn') as HTMLButtonElement;
 
 
-addBook?.addEventListener('click', () => {
-    popUp?.classList.add('active');
-    overlay?.classList.add('active');
-})
+addBookBtn.addEventListener('click', () => {
+    popUp.classList.add('active');
+    overlay.classList.add('active');
+});
 
-overlay?.addEventListener('click', () => {
-    popUp?.classList.remove('active');
+overlay.addEventListener('click', () => {
+    popUp.classList.remove('active');
     overlay.classList.remove('active');
+});
+
+popUpBtn.addEventListener('click', () => {
+    addBook();
 })
 
+let myLibrary: any[] = [];
 
 class Book {
     constructor(
@@ -20,7 +26,22 @@ class Book {
         public title: string,
         public genre: string,
         public numOfPages: number,
-        public read: boolean,
+        public readYes: boolean,
+        public readNo: boolean,
     ) {}
 }
 
+function addBook() {
+    const author = document.querySelector('#author') as HTMLInputElement;
+    const title = document.querySelector('#title') as HTMLInputElement;
+    const genre = document.querySelector('#pageNum') as HTMLInputElement;
+    const pageNum = document.querySelector('#pageNum') as HTMLInputElement;
+    const answerYes = document.querySelector('.yes') as HTMLInputElement;
+    const answerNo = document.querySelector('.no') as HTMLInputElement;
+    
+    let object = new Book(author.value,title.value,genre.value,pageNum.valueAsNumber,answerYes.checked,answerNo.checked)
+    myLibrary.push(object);
+    console.log(myLibrary)
+}
+
+// console.log(myLibrary);
