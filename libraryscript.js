c26d579d5eed11909144cf56797959d8e4870f89
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-05-28 21:59:29 +0200

    temporary way of adding things into the library

--- libraryscript.js
+++ libraryscript.js
@@ -3,6 +3,7 @@ const popUp = document.querySelector('.pop-up');
 const overlay = document.querySelector('.overlay');
 const addBookBtn = document.querySelector('.add-book');
 const popUpBtn = document.querySelector('.pop-up-btn');
+const libraryCardContainer = document.querySelector('section:nth-child(2)');
 addBookBtn.addEventListener('click', () => {
     popUp.classList.add('active');
     overlay.classList.add('active');
@@ -13,6 +14,22 @@ overlay.addEventListener('click', () => {
 });
 popUpBtn.addEventListener('click', () => {
     addBook();
+    const bookCardContainer = document.createElement('div');
+    const author = document.createElement('h3');
+    const title = document.createElement('h3');
+    const genre = document.createElement('h3');
+    const numOfPages = document.createElement('h3');
+    const ReadorNot = document.createElement('h3');
+    author.textContent = myLibrary[0].author;
+    title.textContent = myLibrary[0].title;
+    genre.textContent = myLibrary[0].genre;
+    numOfPages.textContent = myLibrary[0].numOfPages;
+    libraryCardContainer.appendChild(bookCardContainer);
+    bookCardContainer.appendChild(author);
+    bookCardContainer.appendChild(title);
+    bookCardContainer.appendChild(genre);
+    bookCardContainer.appendChild(numOfPages);
+    /*bookCardContainer.appendChild(ReadorNot);*/
 });
 let myLibrary = [];
 class Book {
@@ -28,7 +45,7 @@ class Book {
 function addBook() {
     const author = document.querySelector('#author');
     const title = document.querySelector('#title');
-    const genre = document.querySelector('#pageNum');
+    const genre = document.querySelector('#genre');
     const pageNum = document.querySelector('#pageNum');
     const answerYes = document.querySelector('.yes');
     const answerNo = document.querySelector('.no');
--- libraryscript.ts
+++ libraryscript.ts
@@ -2,6 +2,8 @@ const popUp = document.querySelector('.pop-up') as HTMLDivElement;
 const overlay = document.querySelector('.overlay') as HTMLDivElement;
 const addBookBtn = document.querySelector('.add-book') as HTMLButtonElement;
 const popUpBtn = document.querySelector('.pop-up-btn') as HTMLButtonElement;
+const libraryCardContainer = document.querySelector('section:nth-child(2)') as HTMLDivElement;
+
 
 
 addBookBtn.addEventListener('click', () => {
@@ -16,6 +18,25 @@ overlay.addEventListener('click', () => {
 
 popUpBtn.addEventListener('click', () => {
     addBook();
+
+    const bookCardContainer = document.createElement('div');
+    const author = document.createElement('h3');
+    const title = document.createElement('h3');
+    const genre = document.createElement('h3');
+    const numOfPages = document.createElement('h3');
+    const ReadorNot = document.createElement('h3');
+
+    author.textContent = myLibrary[0].author;
+    title.textContent = myLibrary[0].title;
+    genre.textContent = myLibrary[0].genre;
+    numOfPages.textContent = myLibrary[0].numOfPages;
+
+    libraryCardContainer.appendChild(bookCardContainer);
+    bookCardContainer.appendChild(author);
+    bookCardContainer.appendChild(title);
+    bookCardContainer.appendChild(genre);
+    bookCardContainer.appendChild(numOfPages);
+    /*bookCardContainer.appendChild(ReadorNot);*/
 })
 
 let myLibrary: any[] = [];
@@ -34,7 +55,7 @@ class Book {
 function addBook() {
     const author = document.querySelector('#author') as HTMLInputElement;
     const title = document.querySelector('#title') as HTMLInputElement;
-    const genre = document.querySelector('#pageNum') as HTMLInputElement;
+    const genre = document.querySelector('#genre') as HTMLInputElement;
     const pageNum = document.querySelector('#pageNum') as HTMLInputElement;
     const answerYes = document.querySelector('.yes') as HTMLInputElement;
     const answerNo = document.querySelector('.no') as HTMLInputElement;
